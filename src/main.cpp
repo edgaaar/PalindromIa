a92b3eabf9a02749f37d09ed2a8317e7c3ca9571
edgaaar <edg.700@comunidad.unam.mx>
2016-09-09 00:08:56 -0500

    Casi listo, solo falta encapsular y comentar

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,34 @@ int main()
     std::stack<char> volteada;
     int j = 0;
 
-    for(int  i = 0; palabra[i] != 0;++i){
+    for(int  i = 0; palabra[i] != 0;++i){ //Bloque para quitar espacios a la cadena
         if(palabra[i] != 32){ //32 es espacio en ascii    
             Cadena[j] = palabra[i];
             original.push (Cadena[j]);
+            volteada.push (Cadena[j]);
             j++;
         }
     }
-    cout<<Cadena<<endl;
-    while(!original.empty())
-    {
-        cout << original.front()<< endl;
-        volteada.push(original.front());
-        original.pop();
-        
-    }
 
-    while(!volteada.empty())
+    int iguales = 0;
+    while(!original.empty() && iguales == 0)
     {
-        cout << volteada.top()<< endl;
-        volteada.pop();
+        if (original.front() == volteada.top())
+        {
+            original.pop();
+            volteada.pop();
+        }
+        else{
+            iguales++;
+        }
+
     }
-    
+    if (iguales == 0)
+    {
+        cout << "Palíndromo! :)"<< endl;
+    }
+    else{
+        cout << "No es palíndromo :("<< endl;
+    }
 	return 0;
 }
